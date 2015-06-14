849faaec0721d66f37ca4320030c1ca50e03c4bf
Ricardo Rubio <rickarubio@gmail.com>
2015-06-14 11:44:37 -0700

    Implements token loading and presence checking

--- lib/typekitter/token.rb
+++ lib/typekitter/token.rb
@@ -1,6 +1,12 @@
 module Typekitter
   class Token
+    attr_reader :value
+
     DEFAULT_TOKEN_NAME = '.typekitter_token'
+
+    def initialize(value)
+      @value = value
+    end
 
     def self.token_path
       "#{Dir.pwd}/#{DEFAULT_TOKEN_NAME}"
@@ -10,6 +16,17 @@ module Typekitter
       File.open(token_path, 'w') do |file|
         file.write(value)
       end
+    end
+
+    def self.load
+      if token_exists?
+        value = File.open(token_path).read
+        new value
+      end
+    end
+
+    def self.token_exists?
+      File.exists?(token_path)
     end
   end
 end
--- spec/unit/token_spec.rb
+++ spec/unit/token_spec.rb
@@ -4,7 +4,7 @@ describe Typekitter::Token do
   include FakeFS::SpecHelpers
 
   describe '.save' do
-    describe 'given a string representing the token\'s value' do
+    context 'given a string representing the token\'s value' do
       it 'writes a file to the disk with the given value' do
         expected_token_value = 'my secret token 1234'
 
@@ -14,6 +14,35 @@ describe Typekitter::Token do
         expect(File.open(Typekitter::Token.token_path).read).to eq(
           expected_token_value
         )
+      end
+    end
+  end
+
+  describe '.load' do
+    context 'token file exists' do
+      it 'reads the file, instantiates and returns a token' do
+        expected_token_value = 'foo token 1234'
+
+        Typekitter::Token.save(expected_token_value)
+        token = Typekitter::Token.load
+
+        expect(token.value).to eq(expected_token_value)
+      end
+    end
+  end
+
+  describe '.token_exists?' do
+    context 'token file does not exist in the filesystem' do
+      it 'returns false' do
+        expect(Typekitter::Token.token_exists?).to eq(false)
+      end
+    end
+
+    context 'a token file exists in the filesystem' do
+      it 'returns true' do
+        Typekitter::Token.save('foo bar token')
+
+        expect(Typekitter::Token.token_exists?).to eq(true)
       end
     end
   end
