31f558d2e2a7a24fc023d6c89d060ba62af97291
Ricardo Rubio <rickarubio@gmail.com>
2015-06-15 02:58:58 -0700

    Releases gem to rubygems

--- spec/typekitter_spec.rb
+++ spec/typekitter_spec.rb
@@ -1,11 +1,7 @@
 require 'spec_helper'
 
 describe Typekitter do
-  xit 'has a version number' do
+  it 'has a version number' do
     expect(Typekitter::VERSION).not_to be nil
-  end
-
-  xit 'does something useful' do
-    expect(false).to eq(true)
   end
 end
